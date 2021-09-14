deae335a15aa8eb8103704b58e76f745a5afa352
shifa <shifa.alam@rite.com.bd>
2021-09-14 12:46:23 +0600

    updated

--- CustomerManagementAngular/src/app/app.module.ts
+++ CustomerManagementAngular/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BuildInComponent } from './build-in/build-in.component';
+
 import { HttpClientModule } from '@angular/common/http';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { BuildInComponent } from './components/build-in/build-in.component';
+import { CustomerComponent } from './components/customer/customer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BuildInComponent
+    BuildInComponent,
+    CustomerListComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
